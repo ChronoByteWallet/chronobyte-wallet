b673906d868b11f0b66a28f68c85c048d4dc3a5f
ChronoByteWallet <chronobyte.project@gmail.com>
2025-04-11 07:08:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# ChronoByte Wallet
+
+This is the official front-end wallet app for ChronoByte crypto.
+Built with Vite + React, styled for simplicity and performance.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🔐 ChronoByte Wallet</h1>
+      <p>Welcome to the future of secure digital currency.</p>
+    </div>
+  );
+}
+
+export default App;
+
