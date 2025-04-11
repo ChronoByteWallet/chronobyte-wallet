92ca3d813cf52cec6106b051d939819c5e6f900c
ChronoByteWallet <chronobyte.project@gmail.com>
2025-04-11 06:20:00 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', fontFamily: 'Arial', color: '#fff', background: '#000', height: '100vh' }}>
+      <h1>ChronoByte Wallet</h1>
+      <p>Welcome to the secure next-gen crypto experience.</p>
+    </div>
+  );
+}
+
+export default App;
